5f3feacb0ffab6ee6985add3ca678f18d599fafe
Ukabu <emmanuel@pirsch.org>
2017-03-13 14:26:35 +0000

    fix package script to copy the jar to the output resource

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+set -ex
 
-set -e +x
+pushd attendee-service
+  echo "Packaging JAR"
+  ./mvnw clean package -DskipTests
+popd
 
-cd attendee-service
-./mvnw clean package
+jar_count=`find attendee-service/target -type f -name *.jar | wc -l`
+
+if [ $jar_count -gt 1  ]; then
+  echo "More than one jar found, don't know which one to deploy. Exiting"
+  exit 1
+fi
+
+find attendee-service/target -type f -name *.jar -exec cp "{}" package-output/attendee-service.jar \;
+
+echo "Done packaging"
+exit 0
